8fb7daa17ed3c9db8c5bb21e179448fbb5c121d5
Mathieu Veron <mathieu.veron@adelya.com>
2020-04-21 18:08:02 +0200

    version 0.2 with class definition

--- README.md
+++ README.md
@@ -9,42 +9,47 @@ Usage
 give a array of 6 strings to the drawChords function. Starting with low E string, give the fretted fret.
 indicate a mutted fret with a negative value.
 
-basic
+### basic 
+with a `<div id="gmajor"></div>` in your html, just add this two lines of code
 ```
-var aminor = document.getElementById('aminor'); // aminor is an html canvas
-let aminorctx = aminor.getContext('2d');
-grid(aminorctx);
-drawChord(aminorctx, [-1,0,2,2,1,0]);   // -1 or 'x' for mutted string, 0 for open string
+ var gmajor = new GChord('gmajor');
+ gmajor.draw([3,2,0,0,0,3]);
 ```
 
 for barchord quick notation use fret-. e.g. for G `['3-',5,5,4,3,3]` 3 minus on first string indicate that ther is a bar all accross the neck. bar can start on any string.
 
-using the dictionnary 
+###  using the dictionnary 
 ```
-var cseven = document.getElementById('cseven');
-let csevenctx = cseven.getContext('2d');
-grid(csevenctx);
-drawChord(csevenctx, gChords.open.C7);
+var cseven = new GChord('cseven','C7');
+cseven.draw(gChords.open.C7)
+```
+### css
+generated html is very simple and allow easy css manipulation 
+```
+<div id="gmajor" class="chorddiv"><h3></h3><canvas width="140" height="140"></canvas></div>
 ```
 
+### going further
 Basic support for bar chord.
 Basic support for larger/smaller canvas by changing options
-
 see example/index.html for complete code
+
 
 ![output sample](example/out.png)
 
 Roadmap
 -------
 * enhance predefined common chords in dictionary
-* propose a class definition
 * propose a high level module (with chord and text)
-* support for upper position on the next (e.g. A barchord start on 5 fret)
+* support for upper position on the neck (e.g. A barchord start on 5 fret)
 * propose mini-barchord, for powerchord for example.
+* enable full auto-sizing based on canvas width 
 
+* propose a class definition - done
 * support bar chord in quick chord methods (drawChord) - done
 * be able to adapt to other canvas size - done 
 
 Versions 
 --------
 0.1 - simple low level function, basic dictionary, options, barchords
+0.2 - class oriented code
--- src/guitarchords.js
+++ src/guitarchords.js
@@ -7,128 +7,149 @@ Draw an empty grid
 class GChord {
 
   static defaultOptions = {
-    canvas : {
-    width : 140 ,height:140},
-    grid : {
-        visible : true,
-        stringSpace : 20
+    canvas: {
+      width: 140, height: 140
     },
-    note : { radius : 7},
-    x : { width : 6}
-}
+    grid: {
+      visible: true,
+      stringSpace: 20
+    },
+    note: { radius: 7 },
+    x: { width: 6 }
+  }
 
-  constructor(elementId,options)
-  {
-    this.elementId= elementId;
+  elementId;
+  name;
+  options;
+  ctx;
+
+  constructor(elementId, name, options) {
+    this.elementId = elementId;
+    this.name=name;
     this.options = options;
 
-    if (options === undefined) options = defaultOptiont;
+    if (options === undefined) this.options = GChord.defaultOptions;
+    else this.options = {...GChord.defaultOptions,...options}
+    
   }
-}
 
-var options = {
-  canvas : {
-  width : 140 ,height:140},
-  grid : {
-      visible : true,
-      stringSpace : 20
-  },
-  note : { radius : 7},
-  x : { width : 6}
-}
+  draw(chordAsArray) {
+    let chordiv = document.getElementById(this.elementId);    
+    chordiv.setAttribute("class", "chorddiv");
+    let chordtext = document.createElement("h3");
+    if (this.name !== undefined) chordtext.innerHTML = this.name;
+    let canvaschild = document.createElement("canvas");
+    canvaschild.setAttribute("width", this.options.canvas.width);
+    canvaschild.setAttribute("height", this.options.canvas.height);
+    // put every element in dom div > text + canvas
+    chordiv.appendChild(chordtext);
+    chordiv.appendChild(canvaschild);
+    //parent.appendChild(chordiv);
+    // draw on canvas
+    this.ctx = canvaschild.getContext('2d');
+    this.grid();
+    if (chordAsArray !== undefined)    this.drawChord(chordAsArray);
+  }
 
+  grid() {
 
-function grid(ctx) {
-    
-    let ss = options.grid.stringSpace;
+    let ctx = this.ctx;
 
+    let ss = this.options.grid.stringSpace;
+  
     ctx.beginPath();
-    
+  
     // silet
-    ctx.moveTo(ss,ss-1);
-    ctx.lineTo(6*ss,ss-1);
-    ctx.moveTo(ss,ss);
-    ctx.lineTo(6*ss,ss);
+    ctx.moveTo(ss, ss - 1);
+    ctx.lineTo(6 * ss, ss - 1);
+    ctx.moveTo(ss, ss);
+    ctx.lineTo(6 * ss, ss);
     // vertical
-    for (i=0;i<6;i++) {
-     ctx.moveTo(i*ss+ss,ss);
-     ctx.lineTo(i*ss+ss,6*ss);
+    for (let i = 0; i < 6; i++) {
+      ctx.moveTo(i * ss + ss, ss);
+      ctx.lineTo(i * ss + ss, 6 * ss);
     }
     // horizontal
-    for (i=0;i<6;i++) {
-     ctx.moveTo(ss,i*ss+ss);
-     ctx.lineTo(6*ss,i*ss+ss);
-    }        
-
+    for (let i = 0; i < 6; i++) {
+      ctx.moveTo(ss, i * ss + ss);
+      ctx.lineTo(6 * ss, i * ss + ss);
+    }
+  
     ctx.closePath();
     ctx.stroke();
   }
-
+  
   /*
   Draw a rounded shape on given string and fret
   */
-  function drawNote(ctx,string,fret)
-  {
-    let ss = options.grid.stringSpace; 
-   var radius = options.note.radius;
+  drawNote(string, fret) {
+    let ss = this.options.grid.stringSpace;
+    var radius = this.options.note.radius;
+    let ctx = this.ctx;
 
     ctx.beginPath();
-    ctx.arc(string*ss+ss, fret*ss+ss/2, radius, 0,2*Math.PI, true);
-    if (fret === 0)  {  // unfretted string
-        ctx.stroke();            
+    ctx.arc(string * ss + ss, fret * ss + ss / 2, radius, 0, 2 * Math.PI, true);
+    if (fret === 0) {  // unfretted string
+      ctx.stroke();
     }
     else ctx.fill();
     ctx.closePath();
   }
-
+  
   /*
   Draw an X for mutted string
   */
-  function drawX(ctx, string) {
-    let ss = options.grid.stringSpace; 
-    let xw = options.x.width;
-      ctx.beginPath();
-      ctx.moveTo(string*ss+ss-xw,ss/2-xw);
-      ctx.lineTo(string*ss+ss+xw,ss/2+xw);
-      ctx.moveTo(string*ss+ss+xw,ss/2-xw);
-      ctx.lineTo(string*ss+ss-xw,ss/2+xw);
-      ctx.stroke();  
-      ctx.closePath();
-  }
+  drawX(string) {
 
-   /*
+    let ss = this.options.grid.stringSpace;
+    let xw = this.options.x.width;
+    let ctx = this.ctx;
+
+    ctx.beginPath();
+    ctx.moveTo(string * ss + ss - xw, ss / 2 - xw);
+    ctx.lineTo(string * ss + ss + xw, ss / 2 + xw);
+    ctx.moveTo(string * ss + ss + xw, ss / 2 - xw);
+    ctx.lineTo(string * ss + ss - xw, ss / 2 + xw);
+    ctx.stroke();
+    ctx.closePath();
+  }
+  
+  /*
   Draw a rounded shape on given string and fret
   */
- function drawBar(ctx,string,fret)
- {
-    let ss = options.grid.stringSpace; 
-   var radius = options.note.radius;          
-   ctx.beginPath();
-   ctx.arc(string*ss+ss, fret*ss+ss/2, radius, 0,2*Math.PI, true);
-   ctx.fillRect(string*ss+ss,fret*ss+ss/2-radius,(5-string)*ss,2*radius);
-   ctx.arc(5*ss+ss, fret*ss+ss/2, radius, 0,2*Math.PI, true);
-   ctx.fill();
-   ctx.closePath();
- }
+  drawBar(string, fret) {
+    let ss = this.options.grid.stringSpace;
+    var radius = this.options.note.radius;
+    let ctx = this.ctx;
 
-   /*
+    ctx.beginPath();
+    ctx.arc(string * ss + ss, fret * ss + ss / 2, radius, 0, 2 * Math.PI, true);
+    ctx.fillRect(string * ss + ss, fret * ss + ss / 2 - radius, (5 - string) * ss, 2 * radius);
+    ctx.arc(5 * ss + ss, fret * ss + ss / 2, radius, 0, 2 * Math.PI, true);
+    ctx.fill();
+    ctx.closePath();
+  }
+  
+  /*
   Draw a whole chord
   allnotes is an array of integer
   with -1 for mutted string, 0 for open string, fret value
   e.g. : [-1,0,2,2,1,0]
   */
-  function drawChord(ctx,allnotes) {
-      for (i=0;i<6;i++)
-      {
-          let fret = allnotes[i];
-          // check if there is a notation like '2-' meaning bar on 2nd fret
-          if (typeof(fret)==='string' && fret.includes("-")) {
-            let fretbar = fret.replace('-','');
-            console.log('fret:'+fret);
-            drawBar(ctx,i,fretbar);
-          }
-          else if (fret>=0) drawNote(ctx,i,fret);
-          else drawX(ctx,i);
+  drawChord(allnotes) {
+
+    for (let i = 0; i < 6; i++) {
+      let fret = allnotes[i];
+      // check if there is a notation like '2-' meaning bar on 2nd fret
+      if (typeof (fret) === 'string' && fret.includes("-")) {
+        let fretbar = fret.replace('-', '');      
+        this.drawBar(i, fretbar);
       }
+      else if (fret >= 0) this.drawNote(i, fret);
+      else this.drawX(i);
+    }
   }
-  
+
+}
+
+
